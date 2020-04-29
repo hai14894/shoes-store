be0fae29a2310153f9c33ba7f577e8768bd8403f
hai14894 <hohoanghai14894@gmail.com>
2020-04-29 03:55:17 -0700

    finished CRUD

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -9,14 +9,17 @@ class ShoesController < ApplicationController
     end
 
     def create
-
+        new_shoe = Shoe.create(brand: params["brand"], model: params["model"], condition: params["condition"], price: params["price"],description: params["description"])
+        redirect_to home_path
     end
 
     def update
+        updated_shoe = Shoe.update(params["id"],brand: params["brand"], model: params["model"], condition: params["condition"], price: params["price"],description: params["description"])
 
     end
     
-    def destroy
+    def destroy 
+        destroyed_shoe =choose_shoe.destroy
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get "/shoes", to: "shoes#index" 
+get "/shoes", to: "shoes#index", as: "home"
 get "/shoes/:id", to: "shoes#show"
 post "/shoes", to: "shoes#create"
 put "/shoes/:id", to: "shoes#update"
